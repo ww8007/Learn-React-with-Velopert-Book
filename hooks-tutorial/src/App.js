5cb88fc9aa0da47fe69db62791fe3257ba44030f
ww8007 <wshmin@naver.com>
2021-01-27 20:46:20 +0900

    Use GetAvgReducer

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Counter from './Counter';
 import GetAvg from './GetAvg';
 import GetAvgReducer from './GetAvgReducer';
 import Info from './Info';
 
 const App = () => {
-  return <GetAvg></GetAvg>;
+  return <GetAvgReducer></GetAvgReducer>;
 };
 
 export default App;
