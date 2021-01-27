13def17c5756c26b24878911dbd9b25582b54655
ww8007 <wshmin@naver.com>
2021-01-27 20:47:04 +0900

    Make Success useState->useReducer

--- hooks-tutorial/src/GetAvgReducer.js
+++ hooks-tutorial/src/GetAvgReducer.js
@@ -1,47 +1,75 @@
 import React, { useReducer, useMemo } from 'react';
 
+function getAvg(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+  const sum = numbers.reduce((acc, current) => acc + current);
+  return sum / numbers.length;
+}
+
 function reducer(state, action) {
   switch (action.type) {
-    case 'CHANGE': {
-      return (state.name = action.value);
-    }
     case 'CLICK': {
-      if (action.value === null) return;
-      const list = state.list.concat(parseInt(action.value));
-      if (list.length === 0) {
-        return;
-      }
-      const sum = list.reduce((acc, current) => acc + current);
-      return sum / list.length;
+      console.log('click' + action.list);
+      console.log('click' + action.number);
+      console.log('click' + action);
+      console.log('click' + action.value);
+      console.log('click' + action.name);
+      return {
+        ...state,
+        list: [...state.list, parseInt(state.number)],
+        // [state.number]: '',
+        // [action.number]: '',
+        number: '',
+      };
     }
-    case 'MAKE': {
-    }
+
     default:
-      return state;
+      console.log('타이핑' + state.list);
+      console.log('타이핑' + state.number);
+      console.log(state);
+      console.log(action);
+      console.log('타이핑' + action.value);
+      console.log('타이핑' + action.name);
+      return {
+        ...state,
+        [action.name]: action.value,
+        // number: action.value,
+        // [state.number]: action.value,
+      };
   }
 }
 
 const GetAvgReducer = () => {
-  const [state, dispatch] = useReducer(reducer, { list: [], number: '' });
+  const initialState = {
+    list: [],
+    number: '',
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   const { list, number } = state;
-  const avg = useMemo(() => dispatch('CLICK'), []);
+  const avg = useMemo(() => getAvg(list), [list]);
 
+  const onChange = (e) => {
+    dispatch(e.target);
+  };
+  const onClick = () => {};
   return (
     <div>
       <h1>{avg}</h1>
       <input
-        type="number"
         placeholder="숫자입력"
-        name="number"
         value={number}
-        onChange={() => dispatch({ type: 'CHANGE' })}
+        name="number"
+        onChange={onChange}
       />
-      <button onClick={() => dispatch({ type: 'CLICK' })}>추가</button>
-      <ul>
-        {list.map((value, index) => (
-          <li key={index}>{value}</li>
-        ))}
-      </ul>
+      <button
+        value={number}
+        name="number"
+        onClick={() => dispatch({ type: 'CLICK' })}
+      >
+        추가하기
+      </button>
     </div>
   );
 };
