82f411a0f2d1cfc1eb91ed931b520d5d8ed238c6
ww8007 <wshmin@naver.com>
2021-01-28 23:05:57 +0900

    TodoList Optimize Finish

--- todo-app/README.md
+++ todo-app/README.md
@@ -53,3 +53,102 @@ const onToggle = useCallback(
 마지막에서 onClick = {()=> onTolggle(id)}로 호출한다.
 
 ### 컴포넌트 성능 최적화
+
+느려지는 원인
+
+1. 자신이 전달되는 props 변경
+1. 자신의 state 변경
+1. 부모 컴포넌트가 리렌더링
+1. forceUpdate 함수 실행 시
+
+지금의 상태는 할 일 1을 체크할 경우 App 컴포넌트의 state가 변경되면서 App 컴포넌트 리렌더링
+-> 고로 자식 요소들 까지 리렌더링
+
+할 일 1 에 대해서는 리렌더링이 맞지만 다른 요소들은 리렌더링을 할 필요가 없음
+
+### React.memo 를 사용한 컴포넌트 최적화
+
+간단한 사용법 컴포넌트를 만들고 감싸기만 하면 됨
+
+```jsx
+export default React.memo(TodoListItem);
+```
+
+### onToggle, onRemove 함수가 바뀌지 않게 하기
+
+지금의 상태는 함수가 계속 업데이트 되면서 memo 만으로는 해결 불가 그러므로 다른 것 적용
+
+1. useState 함수형 업데이트 기능
+1. useReducer
+
+### useState 함수형 업데이트
+
+setTodos 함수를 사용할 때 새로운 상태를 파라미터로 넣어줫음
+고로 새롱누 상태를 파라미터로 넣는 대신 상태 업데이트를 어떻게 할지 정의해 주는 업데이트 함수 가능
+
+- 함수를 어떻게 업데이트 할 지 정의 시켜주면 됨
+
+```jsx
+const [number, setNumber] = useState(0);
+
+const onIncrease = useCallback(()=> {
+    setNumber(prevNumber => prevNumber +1);
+}, []),
+}
+
+```
+
+todos에 대해서 todo를 concat 시켜서 사용한다고 정의
+
+```jsx
+setTodos((todos) => todos.concat(todo));
+```
+
+### 프로덕션 모드 구동
+
+yarn build
+yarn global add serve
+serve -s build
+
+### useReducer 사용
+
+useReducer의 두번 째 인자로 undefined를 사용 하고 세번 째 인자로 인자 설정 함수 설정시
+컴포넌트가 맨 처음 랜더링 될 때에만 호출된다.
+
+useReducer를 사용할 경우 기존 코드를 많이 고쳐야 한다는 단점이 존재하지만 상태를 업데이트하는 로직을 모아서 컴포넌트 바깥에 둘 수 있다는 장점이 있다.
+성능은 비슷하기에 취향에 따라서 정의
+
+### 불변성의 중요성
+
+useState를 통해 만든 todos 배열과 setTodos 함수를 사용하는 부분에서
+React.memo를 사용하였을 때 props가 바뀌었는지 혹은 바뀌지 않았는지에 대해 리렌더링 성능 최적화가 가능하다.
+
+### 리스트 최적화
+
+리스트에 대한 최적화를 진행할 때에는 리스트 내부에서 사용하는 컴포넌트도 최적화를 해야하고
+리스트로 사용되는 컴포넌트 자체도 최적화를 해야한다.
+
+TodoList 쪽의 react.memo를 사용한 최적화는 todos 배열이 업데이트 될 때에만 이루어진다.
+지금 TodoList에는 불필요한 리렌더링이 일어나지 않음
+
+- 혹시 App 컴포넌트에 다른 state가 추가되어 해당 값들이 업데이트 될 때 불필요한 리렌더링이 일어날 수 있기에 이에 대한 예방으로 해준 것 이다.
+
+### react-virtualized 를 통한 랜더링 최적화
+
+스크롤 되기전에는 보이는 부분만 랜더링 하도록 한다.
+
+yarn add react-virtualized
+
+reducer 함수 같이 함수를 작성해줘야 한다. 차이점은 컴포넌트 외부에 설정하는 것이 아니라는 것
+
+- List 컴포넌트에서 TodoItem을 랜더링 할 때 사용 List 컴포넌트의 props로 설정해야 함
+  파라미터에 index, key, style 값을 객체로 받아와서 사용
+
+List 컴포넌트
+
+1. 전체 크기
+1. 각 항목 높이
+1. 랜더링 할 때 사용되는 함수
+1. 배열을 props로 넣어줌
+
+css 의 nth child와 컴포넌트 사이 설정도 잊지 말 것
--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,66 +1,75 @@
 import './App.css';
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useReducer } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 function createBult() {
   const array = [];
-  for (let i=)
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'DELETE':
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리엑트 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어보기',
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBult);
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리엑트 기초 알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링 해보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정 관리 앱 만들어보기',
+  //     checked: false,
+  //   },
+  // ]);
+  // const [todos, setTodos] = useState(createBult);
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
       checked: false,
-    },
-  ]);
+    };
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1;
+  }, []);
 
-  const nextId = useRef(4);
+  const onRemove = useCallback((id) => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
 
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos]
-  );
-
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos]
-  );
-
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo
-        )
-      );
-    },
-    [todos]
-  );
+  const onToggle = useCallback((id) => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
