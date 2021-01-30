978cd4114e2a11677e2d99f9b1e053fb59c43921
ww8007 <wshmin@naver.com>
2021-01-30 19:25:23 +0900

    Setting Files

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,38 +1,63 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Todos from '../components/Todos';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
+import useActions from '../lib/useActions';
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }));
 
-const TodosContainer = ({
-  input, //인풋에 들어가는 텍스트
-  todos, //할 일 목록이 들어 있는 객체
-  changeInput,
-  insert,
-  toggle,
-  remove,
-}) => {
+  const [onChangeInput, onInsert, onRemove, onToggle] = useActions(
+    [changeInput, insert, toggle, remove],
+    []
+  );
+
   return (
     <Todos
       input={input}
       todos={todos}
-      onChangeInput={changeInput}
-      onInsert={insert}
-      onToggle={toggle}
-      onRemove={remove}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onRemove={onRemove}
+      onToggle={onToggle}
     ></Todos>
   );
 };
 
-export default connect(
-  // 비구조화 할당을 통해 todos 분리
-  ({ todos }) => ({
-    input: todos.input,
-    todos: todos.todos,
-  }),
-  {
-    changeInput,
-    insert,
-    toggle,
-    remove,
-  }
-)(TodosContainer);
+export default React.memo(TodosContainer);
+
+// const TodosContainer = ({
+//   input, //인풋에 들어가는 텍스트
+//   todos, //할 일 목록이 들어 있는 객체
+//   changeInput,
+//   insert,
+//   toggle,
+//   remove,
+// }) => {
+//   return (
+//     <Todos
+//       input={input}
+//       todos={todos}
+//       onChangeInput={changeInput}
+//       onInsert={insert}
+//       onToggle={toggle}
+//       onRemove={remove}
+//     ></Todos>
+//   );
+// };
+
+// export default connect(
+//   // 비구조화 할당을 통해 todos 분리
+//   ({ todos }) => ({
+//     input: todos.input,
+//     todos: todos.todos,
+//   }),
+//   {
+//     changeInput,
+//     insert,
+//     toggle,
+//     remove,
+//   }
+// )(TodosContainer);
