f4091bf6df7a318b34e4f06744a72f85598dd415
ww8007 <wshmin@naver.com>
2021-02-01 02:56:37 +0900

    Use saga

--- learn-redux-middleware/src/moudules/index.js
+++ learn-redux-middleware/src/moudules/index.js
@@ -1,6 +1,17 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
+import counter, { counterSaga } from './counter';
 import sample from './sample';
-const rootReducer = combineReducers({ counter, sample });
+import loading from './loading';
+import { all } from 'redux-saga/effects';
+const rootReducer = combineReducers({
+  counter,
+  sample,
+  loading,
+});
+
+export function* rootSaga() {
+  //all 여러 사가를 합쳐주는 역할
+  yield all([counterSaga()]);
+}
 
 export default rootReducer;
